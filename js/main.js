9bddc1f387b17ad17be3b4e35cb6a3bc174d92aa
Tea <e12325250@student.tuwien.ac.at>
2026-05-15 02:46:19 +0200

    finalized contact

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit',function(e){
+    e.preventDefault();
+    alert('Thank you! We will be in touch soon ☕✈️') ;
+    this.reset();
+});
